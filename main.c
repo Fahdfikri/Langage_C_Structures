f7a97d5899a6b35bc976f1bc3482ee301bcc1786
fahd fikri <fikrifahd636@gmail.com>
2026-09-24 21:29:26 +0100

    add

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
+#define TMAX 100 
+#define TITRE_LEN 50
+#define ISBN_LEN 8
+
+typedef struct
+{
+    char titre[TITRE_LEN];
+    int isbn[ISBN_LEN];
+} Livre;
+
+Livre mediatheque[TMAX];
+int nbLivres = 0;
+
+// Question 2.a: Fonction d'initialisation
+void init_mediatheque() {
+    nbLivres = 0;
+}
 
 int main() {
-    printf("Bienvenue danss le projet Structures de Donnees en C!\n");
+    init_mediatheque();
+    printf("Mediatheque initialisee. Nombre de livres: %d\n", nbLivres);
     return 0;
 }
